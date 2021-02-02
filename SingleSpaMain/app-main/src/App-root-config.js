5ecfff1989e397414d6b5cad9c1525fa10450640
Wojciech Kaliszewski <wkaliszewski@sii.pl>
2021-02-02 13:59:59 +0100

    Add json

--- SingleSpaMain/app-main/src/App-root-config.js
+++ SingleSpaMain/app-main/src/App-root-config.js
@@ -1,4 +1,29 @@
 import { registerApplication, start } from "single-spa";
+let data = [
+  {
+      "name": "@App/navbar",
+      "app": "@App/navbar",
+      "activeWhen": "/"
+  },
+  {
+      "name": "@App/homepage",
+      "app": "http://localhost:8002/App-homepage.js",
+      "activeWhen": "/"
+  },
+  {
+      "name": "@App/section",
+      "app": "http://localhost:8002/App-section.js",
+      "activeWhen": "/"
+  }
+]
+
+data.forEach(function(item) {
+  registerApplication({
+    name: item.name,
+    app: () => System.import(item.app),
+    activeWhen: [item.activeWhen]
+  });
+});
 
 // registerApplication({
 //   name: "@single-spa/welcome",
@@ -9,23 +34,25 @@ import { registerApplication, start } from "single-spa";
 //   activeWhen: ["/"],
 // });
 
-registerApplication({
-  name: "@App/navbar",
-  app: () => System.import("@App/navbar"),
-  activeWhen: ["/"]
-});
+// registerApplication({
+//   name: "@App/navbar",
+//   app: () => System.import("@App/navbar"),
+//   activeWhen: ["/"]
+// });
 
-registerApplication({
-  name: "@App/homepage",
-  app: () => System.import("http://localhost:8002/App-homepage.js"),
-  activeWhen: ["/"]
-});
+// registerApplication({
+//   name: "@App/homepage",
+//   app: () => System.import("http://localhost:8002/App-homepage.js"),
+//   activeWhen: ["/"]
+// });
 
-registerApplication({
-  name: "@App/section",
-  app: () => System.import("http://localhost:8003/App-section.js"),
-  activeWhen: ["/"]
-});
+// registerApplication({
+//   name: "@App/section",
+//   app: () => System.import("http://localhost:8003/App-section.js"),
+//   activeWhen: ["/homepage"]
+// });
+
+
 
 start({
   urlRerouteOnly: true,
